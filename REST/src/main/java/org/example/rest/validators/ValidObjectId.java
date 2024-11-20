a0860efbd4d6b7d26a0f140b495080c8afdc14a1
obryja <bryja.ola@gmail.com>
2024-11-20 19:12:29 +0100

    REST

---
+++ REST/src/main/java/org/example/rest/validators/ValidObjectId.java
@@ -0,0 +1,17 @@
+package org.example.rest.validators;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = ObjectIdValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidObjectId {
+    String message() default "Niepoprawny format id, powinien mieć 24 znaki w hex";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
